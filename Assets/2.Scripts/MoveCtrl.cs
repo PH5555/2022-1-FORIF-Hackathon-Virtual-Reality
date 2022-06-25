8c37a317ebf305c279a890dc406cae4592f0ad93
PH5555 <lmkn5342@gmail.com>
2022-06-25 10:53:03 +0900

    init

---
+++ Assets/2.Scripts/MoveCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCtrl : MonoBehaviour
+{
+    private Transform camTr;
+    private CharacterController cc;
+    
+    public float speed = 1.0f;
+
+    void Awake()
+    {
+        camTr = Camera.main.GetComponent<Transform>();
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveLookAt();
+    }
+
+    void MoveLookAt(){
+        Vector3 dir = camTr.TransformDirection(Vector3.forward);
+        cc.SimpleMove(dir * speed);
+    }
+}
